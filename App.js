2035f48741130699fccdb51a7f1a1aebb26056c2
GEONHUI LEE <lee@GEONHUIui-MacBook-Pro.local>
2018-08-16 23:55:10 +0900

    commit

--- App.js
+++ App.js
@@ -32,7 +32,7 @@ export default class App extends React.Component {
 
   // 이벤트 등록
   componentDidMount() {
-    console.log("mounted123");
+    console.log("mounted");
     BackHandler.addEventListener('hardwareBackPress', this.handleBackButton);
 
     BackHandler.addEventListener('hardwareBackPress', this.handleBackButton);
